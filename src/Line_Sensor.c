ba678e62aafc89c106bad9e918543b6519e18e34
Justin <jul155@pitt.edu>
2024-07-06 03:44:50 -0400

    Created Skeleton for the line sensor code.

---
+++ src/Line_Sensor.c
@@ -0,0 +1,5 @@
+#include <Arduino.h>
+#include <stdint.h>
+#include "Line_Sensor.h"
+
+
---
+++ src/Line_Sensor.h
@@ -0,0 +1,8 @@
+#ifndef LINE_SENSOR_H
+#define LINE_SENSOR_H
+
+void Line_Sensor_Init(void);
+void Line_Sensor_Start(void);
+void Line_Sensor_Stop(void);
+
+#endif
